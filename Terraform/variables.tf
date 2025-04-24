6600e6ab26e9e9a5bad7fc265c5b5beaee3f1e70
D4RKOPS <102692790+D4RKOPS@users.noreply.github.com>
2025-04-24 10:38:50 -0500

    terraform infra

---
+++ Terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "resource_group_name" { default = "rg-microservices" }
+variable "location"            { default = "East US" }
+variable "acr_name"            { default = "acrmicroservices" }
+variable "aks_name"            { default = "aksmicroservices" }
+variable "redis_name"          { default = "redismicroservices" }
